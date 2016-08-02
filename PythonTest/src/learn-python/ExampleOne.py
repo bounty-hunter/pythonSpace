9d93d57bf1c54a4bc269450200fed0ba3286858a
Debanjan Sarkar <debanjan02sarkar@outlook.com>
2016-08-03 00:21:30 +0530

    init Check-In

---
+++ PythonTest/src/learn-python/ExampleOne.py
@@ -0,0 +1,10 @@
+'''
+Created on Jul 2, 2016
+
+@author: bounty_hunter
+'''
+name = input("Enter your name ")
+age = input("Enter your age")
+for i in range(0,100,1):
+    print(name)
+    print(age)
---
+++ PythonTest/src/learn-python/pytest.py
@@ -0,0 +1,8 @@
+'''
+Created on Jun 27, 2016
+
+@author: bounty_hunter
+'''
+
+ide = "Eclipse"
+print(ide)
