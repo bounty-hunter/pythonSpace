b0457d975e2dafa7b50da2c09902fc5cbb0845b1
realKakarotto <debanjan02sarkar@outlook.com>
2018-05-10 00:06:40 +0530

    JacocoTest Project

---
+++ JacocoTest/src/main/java/math/operation/Application.java
@@ -0,0 +1,11 @@
+package math.operation;
+
+public class Application {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Going to sleep");
+        Thread.sleep(20000);
+        System.out.println("Woken from sleep");
+    }
+
+}
